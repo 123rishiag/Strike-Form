3bf34833da76990e9c7aebba9f2cca5631488a66
Rishabh Garg <narishabh.garg@gmail.com>
2025-04-18 01:08:51 +0530

    Created Basic Project

---
+++ Source/StrikeForm/StrikeForm.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StrikeForm.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StrikeForm, "StrikeForm" );
