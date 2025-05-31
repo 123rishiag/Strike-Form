2f8171a0951f3884f41f612490942d96665c998e
Rishabh Garg <narishabh.garg@gmail.com>
2025-05-31 19:02:41 +0530

    Created Weapon Classes and Weapon Axe Blueprint

---
+++ Source/StrikeForm/Public/Items/Weapons/WarriorHeroWeapon.h
@@ -0,0 +1,17 @@
+// Strike Form
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/WarriorWeaponBase.h"
+#include "WarriorHeroWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRIKEFORM_API AWarriorHeroWeapon : public AWarriorWeaponBase
+{
+	GENERATED_BODY()
+	
+};
